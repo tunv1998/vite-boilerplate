8e50058828627b3b3a2c009a273f729794cdd141
Tu Nguyen <nguyenvantu191298@gmail.com>
2022-08-15 18:21:52 +0700

    VT-01: re-config eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,18 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
   overrides: [],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: ['./tsconfig.json'],
   },
-  plugins: ['react'],
+  plugins: ['react', '@typescript-eslint'],
   rules: {},
 };
