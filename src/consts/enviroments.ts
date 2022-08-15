d71c3e5b3a4353cc927a99152cb94dca36f02a86
Tu Nguyen <nguyenvantu191298@gmail.com>
2022-08-15 22:16:42 +0700

    VT-01: setup base

---
+++ src/consts/enviroments.ts
@@ -0,0 +1,3 @@
+export const ENVIROMENTS = {
+  BASE_API_URL: import.meta.env.VITE_BASE_API_URL,
+};
--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,9 @@ export default defineConfig({
   resolve: {
     alias: [{ find: 'src', replacement: path.resolve(__dirname, 'src') }],
   },
+  test: {
+    global: true,
+    enviroment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+  },
 });
